eb2c1297214d1a724ef1093f1279501b973dd83f
flutterWang <wangbofeng980404@gmail.com>
2020-04-10 01:44:35 +0800

    add client basic

--- basic/client/main.go
+++ basic/client/main.go
@@ -1,32 +1,37 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/flutterWang/learningRPC/basic/proto/csv"
+	"google.golang.org/grpc"
 )
 
-var (
-	db *sql.DB
-	dburl = "root:111111@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local&allowAllFiles=true"
-	sqlString = `LOAD DATA INFILE "./electricData.csv" INTO TABLE electrion FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n'  IGNORE 1 LINES`
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
 )
-
-func init() {
-	var err error
-	db, err = sql.Open("mysql", dburl)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func initTable() {
-	
-}
 
 func main() {
-	_, err := db.Exec(sqlString)
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("did not connect: %v", err)
 	}
+	defer conn.Close()
+	c := pb.NewCsvClient(conn)
+
+	// Contact the server and print out its response.
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.ImportData(ctx, &pb.ImportDataRequest{FileName: "./electricData.csv", TableName: "electrion"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	log.Printf("Greeting: %s", r.GetMessage())
 }
